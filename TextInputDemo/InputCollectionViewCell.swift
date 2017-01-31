aeda5a9a1ef6219f9b3ed620bfadb2e5892ef0f3
DianQK <xiaoqing@dianqk.org>
2017-01-31 14:21:11 +0800

    add base demo

--- TextInputDemo/InputCollectionViewCell.swift
+++ TextInputDemo/InputCollectionViewCell.swift
@@ -7,9 +7,61 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class InputCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textField: InputTextField!
+
+    var isInputing: UIBindingObserver<InputCollectionViewCell, Bool> {
+        return UIBindingObserver(UIElement: self, binding: { (UIElement, value) in
+            UIElement.textField._canBecomeFirstResponder = value
+            if value {
+                _ = UIElement.becomeFirstResponder()
+            } else {
+                _ = UIElement.resignFirstResponder()
+            }
+        })
+    }
+
+    var canInput: UIBindingObserver<InputCollectionViewCell, Bool> {
+        return UIBindingObserver(UIElement: self, binding: { (UIElement, value) in
+            UIElement.textField._canBecomeFirstResponder = value
+        })
+    }
+
+    override func becomeFirstResponder() -> Bool {
+        return textField.becomeFirstResponder()
+    }
+
+    override var canBecomeFirstResponder: Bool {
+        return textField.canBecomeFirstResponder
+    } 
+
+    override var canResignFirstResponder: Bool {
+        return textField.canResignFirstResponder
+    }
+
+    override func resignFirstResponder() -> Bool {
+        return textField.resignFirstResponder()
+    }
+
+    override var isFirstResponder: Bool {
+        return textField.isFirstResponder
+    }
+
+    private(set) var reuseDisposeBag = DisposeBag()
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        textField.text = nil
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        textField.text = nil
+        reuseDisposeBag = DisposeBag()
+    }
 
 }
--- TextInputDemo/ViewController.swift
+++ TextInputDemo/ViewController.swift
@@ -12,8 +12,8 @@ import RxCocoa
 import RxDataSources
 
 struct InputModel {
-    let text = Variable("")
-    let isFirstResponder = Variable(false)
+    let inputText: Variable<String>
+//    let isFirstResponder = Variable(false)
 }
 
 typealias InputSectionModel = SectionModel<String, InputModel>
@@ -23,10 +23,64 @@ class ViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var label: UILabel!
 
-    let dataSource = RxTableViewSectionedReloadDataSource<InputSectionModel>()
+    let dataSource = RxCollectionViewSectionedReloadDataSource<InputSectionModel>()
+
+    let inputText = Variable("")
+
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        inputText.asObservable()
+            .bindTo(label.rx.text)
+            .addDisposableTo(disposeBag)
+
+        dataSource.configureCell = { dataSource, collectionView, indexPath, element in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "InputCollectionViewCell", for: indexPath) as! InputCollectionViewCell
+
+            cell.textField.rx.controlEvent(.editingChanged)
+                .map { cell.textField.text ?? "" }
+                .filter { !$0.isEmpty }
+                .subscribe(onNext: { text in
+                    element.inputText.value += text
+                })
+                .addDisposableTo(cell.reuseDisposeBag)
+
+            element.inputText.asObservable()
+                .map({ (text) -> String in
+                    if text.characters.count > indexPath.row {
+                        return String(text.characters.map { $0 }[indexPath.row])
+                    } else {
+                        return ""
+                    }
+                })
+                .bindTo(cell.textField.rx.text)
+                .addDisposableTo(cell.reuseDisposeBag)
+
+             cell.textField.rx.methodInvoked(#selector(UITextField.deleteBackward))
+                .map { _ in }
+                .subscribe(onNext: {
+                    guard !element.inputText.value.isEmpty else { return }
+                    let removedIndex = element.inputText.value.index(before: element.inputText.value.endIndex)
+                    element.inputText.value.remove(at: removedIndex)
+                })
+                .addDisposableTo(cell.reuseDisposeBag)
+
+            element.inputText.asObservable().map { $0.lengthOfBytes(using: String.Encoding.utf8) }
+                .map { $0 == indexPath.row }
+                .distinctUntilChanged()
+                .bindTo(cell.isInputing)
+                .addDisposableTo(cell.reuseDisposeBag)
+
+            return cell
+        }
+
+        Observable.just([inputText, inputText, inputText, inputText, inputText, inputText])
+            .map { $0.map { InputModel(inputText: $0) } }
+            .map { [InputSectionModel(model: "", items: $0)] }
+            .bindTo(collectionView.rx.items(dataSource: dataSource))
+            .addDisposableTo(disposeBag)
 
     }
 
